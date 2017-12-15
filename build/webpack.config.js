3280aaf7001f6734339e50ad138f2b2016e82fc4
dj940212 <2902273280@qq.com>
2017-12-15 16:39:28 +0800

    配置loader打包js文件,配置html-webpack-plugin成功打包

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+
 module.exports = {
 	entry: {
 		app: path.join(__dirname, '../client/app.js')
@@ -6,14 +8,23 @@ module.exports = {
 	output: {
 		filename: '[name]-[hash].js',
 		path: path.join(__dirname, '../dist'),
-		publicPath: '/public'
+		publicPath: ''
 	},
 	module: {
 		rules: [
 			{
 				test: /.jsx$/,
 				loader: 'babel-loader'
-			}
+			},{
+				test: /.js$/,
+				exclude: [
+					path.join(__dirname, '../node_modules')
+				],
+				loader: 'babel-loader'
+			}			
 		]
-	}
+	},
+	plugins: [
+		new HTMLPlugin()
+	]
 }
--- client/app.js
+++ client/app.js
@@ -1,4 +1,5 @@
+import React from 'react' //必须引入
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 
-ReactDOM.render(App, document.body)
+ReactDOM.render(<App />, document.body)
