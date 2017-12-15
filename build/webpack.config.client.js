9f44e9e90cf80cd62f5edda8437b69d74e5ae238
dj940212 <2902273280@qq.com>
2017-12-15 21:10:36 +0800

    完成热模块更新配置

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const HTMLPlugin = require('html-webpack-plugin')
 
 
@@ -9,9 +10,9 @@ const config = {
 		app: path.join(__dirname, '../client/app.js')
 	},
 	output: {
-		filename: '[name]-[hash].js',
+		filename: '[name].[hash].js',
 		path: path.join(__dirname, '../dist'),
-		publicPath: '/public'
+		publicPath: '/public/'             //必须为  /public/ 否则热更新部分文件请求地址出错
 	},
 	module: {
 		rules: [
@@ -36,21 +37,29 @@ const config = {
 
 // dev启动删除本地的dist目录，因为缓存中没有文件， 默认会访问本地的dist目录
 if (isDev) {
+	config.entry = {
+		app: [
+			'react-hot-loader/patch',
+			path.join(__dirname, '../client/app.js')
+		]
+	}
 	config.devServer = {
 		host: '0.0.0.0',
 		port: '8888',
 		contentBase: path.join(__dirname, '../dist'),
-		// hot: true,
+		hot: true,
 		// 显示错误信息
 		overlay: {
 			errors: true
 		},
-		publicPath: '/public',
+		publicPath: '/public/',
 		// 404返回地址
 		historyApiFallback: {
 			index: '/public/index.html'
 		}
 	}
+	// 热模块更新插件
+	config.plugins.push(new webpack.HotModuleReplacementPlugin())
 }
 
 module.exports = config
--- client/app.js
+++ client/app.js
@@ -1,6 +1,28 @@
 import React from 'react' //必须引入
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import App from './App.jsx'
 
 // 对比服务端和客户端代码， 如果有不同，用客户端代码代替服务端
-ReactDOM.hydrate(<App />, document.getElementById('root'))
+// ReactDOM.hydrate(<App />, document.getElementById('root'))
+
+const root = document.getElementById('root')
+const render = Component => {
+	ReactDOM.hydrate(
+		<AppContainer>
+			<Component/>
+		</AppContainer>,
+		root
+	)
+}
+
+render(App)
+
+// 热模块更新
+if (module.hot) {
+	module.hot.accept('./App.jsx', () => {
+		const NextApp = require('./App.jsx').default
+		// ReactDOM.hydrate(<NextApp />, document.getElementById('root'))
+		render(NextApp)
+	})
+}
